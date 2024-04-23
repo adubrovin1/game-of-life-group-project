4bd6198828cef43147dd4980533c337d347d3dea
AYETATAPIN <vendetta227@yandex.ru>
2024-04-23 15:55:31 +0700

    Working button input without showing current position

--- project_codes/joystick.asm
+++ project_codes/joystick.asm
@@ -2,9 +2,9 @@
 # r1 - y shift address
 # r2 - column
 # r3 - y shift
-# r4 - 16 comparison
-# r5 - shifted res
-# r6 - shift count
+# r4 - firstly previous state, secondly 16 comparison
+# r5 - firstly button address, secondly shifted res
+# r6 - firstly button condition, secondly shift count
 
 asect 0x00
 
@@ -13,27 +13,38 @@ dc main, 0x00 # сюда прерывания
 main:
     ldi r0, 0xbabe # column
     ldi r1, 0xbeef # y shift
+    ldi r5, 0xcafe # button adress
+    push r4
     loop:
-    ldw 0xbabe, r2
-    ldw r1, r3
-    ldi r5, 1
-    ldi r6, 0
-    tst r3
+    ldw r5, r6
+    pop r4
+    tst r6
+    push r6
     bz loop
-    ldi r4, 15
     if
-        cmp r3, r4
-    is gt
-        inc r4
-        sub r3, r4, r3
+        cmp r6, r4
+    is ne
+        ldw r0, r2
+        ldw r1, r3
+        ldi r4, 15
+        ldi r5, 1
+        ldi r6, 0
+        if
+            cmp r3, r4
+        is gt
+            inc r4
+            sub r3, r4, r3
+        fi
+        while
+            cmp r3, r6
+        stays gt
+            shl r5, r5, 1
+            inc r6
+        wend
+        xor r2, r5, r2
+        stw r0, r2
+        ldi r5, 0xcafe
+        ldi r6, 0
     fi
-    while   
-        cmp r3, r6
-    stays gt
-        shl r5, r5, 1
-        inc r6
-    wend
-    xor r2, r5, r2
-    stw r0, r2
     br loop
 end.
