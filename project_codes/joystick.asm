1b63b45ad3ad7ecaaaa775b22946f82321ced251
AYETATAPIN <vendetta227@yandex.ru>
2024-04-23 13:03:42 +0700

    Buttons shift calculation fixed?

--- project_codes/joystick.asm
+++ project_codes/joystick.asm
@@ -2,25 +2,32 @@
 # r1 - y shift address
 # r2 - column
 # r3 - y shift
-# r4 - xored column
+# r4 - 16 comparison
 # r5 - shifted res
 # r6 - shift count
 
 asect 0x00
 
-dc main, 0x00
+dc main, 0x00 # сюда прерывания
 
 main:
     ldi r0, 0xbabe # column
     ldi r1, 0xbeef # y shift
     loop:
-    ldw r0, r2
+    ldw 0xbabe, r2
     ldw r1, r3
     ldi r5, 1
     ldi r6, 0
     tst r3
     bz loop
-    while
+    ldi r4, 15
+    if
+        cmp r3, r4
+    is gt
+        inc r4
+        sub r3, r4, r3
+    fi
+    while   
         cmp r3, r6
     stays gt
         shl r5, r5, 1
